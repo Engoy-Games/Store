9fc67a88fa9da738030521f8c932b37c3703c5b2
Your Name <you@example.com>
2024-11-12 00:39:19 +0200

    feat: Update layout and styling for various components, add ProductListThree and CategoryDisplay components

--- components/ServiceBoxes.jsx
+++ components/ServiceBoxes.jsx
@@ -25,11 +25,11 @@ const services = [
 
 const ServiceBoxes = () => {
   return (
-    <div className="flex flex-col sm:flex-row justify-center items-center gap-10 p-6 px-28">
+    <div className="flex flex-col sm:flex-row justify-center items-center gap-10 p-6 px-[80px]">
       {services.map((service) => (
         <div 
           key={service.id} 
-          className="bg-white flex flex-col items-center w-full sm:w-1/3 p-6 shadow-lg rounded-lg text-center border border-gray-100 transition-transform transform hover:scale-105 hover:bg-yellow-50 duration-300"
+          className="bg-white flex flex-col items-center w-full sm:w-1/3 px-6 py-14 shadow-lg rounded-lg text-center border border-gray-100 transition-transform transform hover:scale-105 hover:bg-yellow-50 duration-300"
         >
           {service.icon}
           <h3 className="text-black text-lg font-semibold mb-2">{service.title}</h3>
